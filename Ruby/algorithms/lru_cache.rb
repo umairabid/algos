0cc50340fdf1556efdd45c7e15e70d946129d7aa
Umair Abid <umair.abid@risepeople.com>
2021-09-05 19:59:14 -0700

    added algorithm for lru cache

---
+++ Ruby/algorithms/lru_cache.rb
@@ -0,0 +1,78 @@
+class Node
+  attr_accessor :next, :prev, :val, :key
+
+  def initialize
+    @val = nil
+    @next = nil
+    @prev = nil
+    @key = nil
+  end
+end
+
+class LRUCache
+
+  def initialize(size)
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @size = size
+    @hash = {}
+  end
+
+  def get(key)
+    return if @hash[key].nil?
+    node = @hash[key]
+    remove_node node
+    add_node_with_head node
+    node.val
+  end
+
+  def put(key, value)
+    node = Node.newP
+    node.val = value
+    node.key = key
+    if @hash[key]
+      node = @hash[key]
+      node.val = value
+      remove_node node
+    elsif @hash.length == @size
+      evict_tail_node
+    end
+    
+    add_node_with_head(node)
+    @hash[key] = node
+  end
+
+  private
+
+  def add_node_with_head(node)
+    curr_head_next = @head.next
+    @head.next = node
+    node.prev = @head
+    node.next = curr_head_next
+    curr_head_next.prev = node
+  end
+
+  def evict_tail_node
+    node = @tail.prev
+    @hash.delete node.key
+    remove_node node
+  end
+
+  def remove_node(node)
+    prev = node.prev
+    nxt = node.next
+    prev.next = nxt
+    nxt.prev = prev
+  end
+end
+
+cache = LRUCache.new(2)
+
+cache.get(2)
+cache.put(2,6)
+cache.get(1)
+cache.put(1,5)
+cache.put(1,2)
+puts cache.get(1)
+puts cache.get(2)
